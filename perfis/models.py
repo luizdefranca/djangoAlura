0c74892673ed53f45eef81a75ef60109d1ac9ada
luizdefranca <luizramospe@hotmail.com>
2016-07-26 22:21:26 -0300

    Concluido módulo 4

--- perfis/models.py
+++ perfis/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from email import email
 
-class Perfil(object):
-    def __init__(self, nome='', email='', telefone='', nome_empresa=''):
-        self.nome = nome
-        self.email = email
-        self.telefone = telefone
-        self.nome_empresa = nome_empresa
-        
+
+
+class Perfil(models.Model):
+    nome = models.CharField(max_length=100, null=False)
+    email = models.CharField(max_length=100, null=False)
+    telefone = models.CharField(max_length=15, null=False)
+    nome_empresa = models.CharField(max_length=100, null=False)
+
     def __str__(self):
         return 'nome: %s \t email: %s \t telefone: %s \t empresa: %s' % (self.nome, self.email, self.telefone, self.nome_empresa)
--- perfis/views.py
+++ perfis/views.py
@@ -1,16 +1,22 @@
+# encoding: utf-8
 from django.shortcuts import render
 from perfis.models import Perfil
 
 def index(request):
     return render(request, 'index.html')
+
 def exibir(request, perfil_id):
     print 'ID do perfil recebido: %s' % (perfil_id)
-    perfil = Perfil()
-    
-    if perfil_id == '1':
-        perfil = Perfil('Luiz Carlos', 'luizramos@hotmail.com', '679696', 'TCE')
-    if perfil_id == '2':
-        perfil = Perfil('Romulo Henrique', 'romulo@romulo.com.br', '888888', 'Caelum')
-        
+    perfil = Perfil.objects.get(id = perfil_id)
+
+    # Perfil provisório para testar
+    # if perfil_id == '1':
+    #     perfil = Perfil('Luiz Carlos', 'luizramos@hotmail.com', '679696', 'TCE')
+    # if perfil_id == '2':
+    #     perfil = Perfil('Romulo Henrique', 'romulo@romulo.com.br', '888888', 'Caelum')
+
+    context = {
+        "perfil" : perfil,
+    }
     print perfil
-    return render(request, 'perfil.html', {"perfil" : perfil})
+    return render(request, 'perfil.html', context )
